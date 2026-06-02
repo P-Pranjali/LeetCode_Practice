34336ea5625d171daf7cdc6f9f6e380e076b5269
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-06-02 10:23:00 +0530

    Time: 23 ms (58.89%) | Memory: 47.4 MB (90.69%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,19 +1,16 @@
 class Solution {
     public int minimumDeletions(String s) {
 
-        int deletation =0;
-        int bCount =0;
+        int bCount = 0;
+        int deletation = 0;
 
-        char a = 'a';
-        char b= 'b';
+        for(int i = 0; i < s.length(); i++){
 
-        for(char ch: s.toCharArray()){
-
-            if(ch==b){
+            if(s.charAt(i) == 'b'){
                 bCount++;
             }else{
-            deletation = Math.min(deletation+1, bCount);
-        }
+                deletation = Math.min(bCount, deletation+1);
+            }   
         }
         return deletation;
     }
